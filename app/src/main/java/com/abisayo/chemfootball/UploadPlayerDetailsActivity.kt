3eadb3e573b51a4c40c86400892183a56a1a2872
Abisayo <foluagoiabisayo@gmail.com>
2023-06-12 18:28:08 +0100

    Now the multiplayer screen has been initialized

--- app/src/main/java/com/abisayo/chemfootball/UploadPlayerDetailsActivity.kt
+++ app/src/main/java/com/abisayo/chemfootball/UploadPlayerDetailsActivity.kt
@@ -7,17 +7,18 @@ import android.view.Window
 import android.view.WindowManager
 import android.widget.Toast
 import com.abisayo.chemfootball.data.Constants
-import com.abisayo.chemfootball.databinding.ActivityEnterNameBinding
+import com.abisayo.chemfootball.databinding.ActivityUploadPlayerDetailsBinding
+import com.abisayo.chemfootball.models.Multiplayer
 import com.abisayo.chemfootball.models.Player
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
-class EnterNameActivity : AppCompatActivity() {
+class UploadPlayerDetailsActivity : AppCompatActivity() {
     private lateinit var database : DatabaseReference
     private var clas = "SS1"
     private var player = ""
-    private lateinit var binding: ActivityEnterNameBinding
+    private lateinit var binding: ActivityUploadPlayerDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
@@ -26,35 +27,23 @@ class EnterNameActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
-        binding = ActivityEnterNameBinding.inflate(layoutInflater)
+        binding = ActivityUploadPlayerDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
 
         player = intent.getStringExtra(Constants.PLAYER).toString()
         clas = intent.getStringExtra(Constants.CLASS).toString()
         val keeper = intent.getStringExtra(Constants.KEEPER).toString()
+        val name = intent.getStringExtra(Constants.NAME)
 
-        val name = binding.editText.text
 
+        val computer_name = binding.editTextComputer.text
+        val game_code = binding.editTextGameCode.text
 
-        binding.button.setOnClickListener {
-            if (name.isNotEmpty()) {
-                val intent = Intent(this, GameIntroActivity::class.java)
-                intent.putExtra(Constants.CLASS, clas)
-                intent.putExtra(Constants.KEEPER, keeper)
-                intent.putExtra(Constants.PLAYER, player)
-                intent.putExtra(Constants.NAME, "$name")
-                startActivity(intent)
-            } else {
-                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
-            }
+        binding.editTextName.setText("$name")
 
-        }
-
-        binding.mulyiPlayer.setOnClickListener {
-            if (name.isNotEmpty()) {
-                saveName("$name")
+        binding.btnSubmit.setOnClickListener {
+            if (computer_name.isNotEmpty() && binding.editTextName.text.isNotEmpty()) {
+                saveName("$name", "$computer_name", "$game_code")
                 val intent = Intent(this, UploadPlayerDetailsActivity::class.java)
                 intent.putExtra(Constants.CLASS, clas)
                 intent.putExtra(Constants.KEEPER, keeper)
@@ -63,25 +52,30 @@ class EnterNameActivity : AppCompatActivity() {
                 intent.putExtra(Constants.GAME_MODE, "multi_player")
                 startActivity(intent)
             } else {
-                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Please enter your opponent name", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    fun saveName(name : String, opp_name : String, code: String) {
+        val my_score_status = "0"
+        val opponent_score_status = "0"
+        val my_score = "0"
+        val opponent_score = "0"
+        val trialNum = "0"
+        val  userID = FirebaseAuth.getInstance().currentUser?.uid
+
+        database = FirebaseDatabase.getInstance().getReference("Multiplayer")
+        val multi_player = Multiplayer("$name",opp_name, code,my_score_status, opponent_score_status, my_score, opponent_score, trialNum )
+        if (userID != null) {
+            database.child(code).setValue(multi_player).addOnSuccessListener {
+                Toast.makeText(this, "Successfully Saved", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener {
+                Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    fun saveName(name : String) {
-        val name = name
-        val trialNum = "0-0"
-        val score = "0-0"
-        val  userID = FirebaseAuth.getInstance().currentUser?.uid
 
-        database = FirebaseDatabase.getInstance().getReference("Player")
-        val player = Player("$name",trialNum, score)
-            if (userID != null) {
-                database.child(userID).setValue(player).addOnSuccessListener {
-                    Toast.makeText(this, "Successfully Saved", Toast.LENGTH_SHORT).show()
-                }.addOnFailureListener {
-                    Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
-                }
-            }
-    }
 }
--- app/src/main/java/com/abisayo/chemfootball/models/Courses.kt
+++ app/src/main/java/com/abisayo/chemfootball/models/Courses.kt
@@ -5,3 +5,13 @@ data class Courses(var courseTitle: String ?= null, var note : String ?= null)
 data class Scores(var studentName: String ?= null, var courseTitle: String ?= null, var studentScore : String ?= null, val userId: String?= null)
 data class Questions(var questNum : Int? = null, var question: String ? = null, var option1 : String ?= null, var option2 : String? = null, var option3 : String ?= null, var option4 : String? = null)
 data class Player(var name : String? = null, var trialNum: String ? = null, var score : String ?= null)
+
+data class Multiplayer(
+    var name: String?= null,
+    var opponent_name : String?= null,
+    var code: String?= null,
+    var my_score_status: String?= null,
+    var opponent_score_status: String?= null,
+    var my_score : String? = null,
+    var opponent_score : String?=null,
+    var trialNum: String? = null)
